d605415f5637a5cb42993a9c3bb21f491a3b2482
Apsana7 <apsananeupane124@gmail.com>
2025-05-26 09:16:09 +0545

    resolve conflicts

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -29,7 +29,6 @@ function LandingPage() {
 
   return (
     <div className='w-full h-lvh'>
-<<<<<<< HEAD
       <div className='flex  fixed top-4 w-10/12 px-5 right-0 left-0 mx-auto z-10 items-center justify-center py-6 rounded-md border-b-2 border-gray-100 bg-purple-100'>
         <div className='w-full  flex justify-between items-center mx-auto'>
           <div className='text-3xl font-semibold text-purple-700'>CrackIt</div>
@@ -39,17 +38,6 @@ function LandingPage() {
             <div className="relative" ref={studyToolsRef}>
               <div
                 className="flex items-center gap-2 cursor-pointer"
-=======
-      <div className='flex items-center justify-center py-4 border-b-2 border-gray-100 bg-purple-100'>
-        <div className='w-10/12 flex justify-between items-center mx-auto'>
-          <div className='text-3xl font-semibold text-purple-700'>CrackIt</div>
-
-          <div className='flex w-3/16 justify-between gap-6'>
-          
-            <div className="relative" ref={studyToolsRef}>
-              <div
-                className="flex items-center gap-2 cursor-pointer text-lg font-medium"
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
                 onClick={() => setShowStudyToolsDropDown(prev => !prev)}
               >
                 Study tools
@@ -59,31 +47,18 @@ function LandingPage() {
               {showStudyToolsDropdown && (
                 <div className="absolute top-full mt-2 w-40 bg-white border rounded shadow-md z-10">
                   <ul className="text-sm">
-<<<<<<< HEAD
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Flashcards</li>
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Mock Tests</li>
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Boosters</li>
-=======
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Quiz</li>
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Flashcards</li>
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Ask Ai</li>
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
                   </ul>
                 </div>
               )}
             </div>
 
-<<<<<<< HEAD
-            
-            <div className='relative' ref={facultyRef}>
-              <div
-                className='flex items-center gap-2 cursor-pointer'
-=======
            
             <div className='relative' ref={facultyRef}>
               <div
                 className='flex items-center gap-2 cursor-pointer text-lg font-medium'
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
                 onClick={() => setShowFacultyDropdown(prev => !prev)}
               >
                 Faculty
@@ -93,22 +68,15 @@ function LandingPage() {
               {showFacultyDropdown && (
                 <div className="absolute top-full mt-2 w-40 bg-white border rounded shadow-md z-10">
                   <ul className="text-sm">
-<<<<<<< HEAD
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Science</li>
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Management</li>
-                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Humanities</li>
-=======
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">IOE</li>
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">IOM</li>
                     <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">IAAS</li>
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
                   </ul>
                 </div>
               )}
             </div>
           </div>
 
-<<<<<<< HEAD
           <div className='flex gap-4'>
             <Link to={"/login"}>
               <button className='w-fit h-12 overflow-clip rounded-2xl px-10 border border-purple-500 bg-transparent  group hover:bg-purple-600 hover:text-white transition-all duration-1000 delay-75 ease-in-out text-purple-500 cursor-pointer'>
@@ -120,16 +88,6 @@ function LandingPage() {
             </Link>
             <Link to={'/SignUp'}>
               <button className='w-fit h-fit rounded-2xl p-3 px-10 bg-purple-500 text-white hover:bg-purple-700 cursor-pointer'>
-=======
-          <div className='flex justify-between w-3/20 gap-4'>
-            <Link to={"/login"}>
-              <button className='w-fit h-fit rounded-2xl p-2 border border-gray-500 bg-white hover:bg-gray-300 hover:text-white cursor-pointer'>
-                Login
-              </button>
-            </Link>
-            <Link to={'/SignUp'}>
-              <button className='w-fit h-fit rounded-2xl p-2 bg-purple-500 text-white hover:bg-purple-700 cursor-pointer'>
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
                 Sign Up
               </button>
             </Link>
@@ -138,21 +96,13 @@ function LandingPage() {
       </div>
 
    
-<<<<<<< HEAD
       <div className="relative w-full h-full">
-=======
-      <div className="relative w-full h-screen">
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
         <img
           src={BannerImage}
           alt="banner"
           className="absolute inset-0 w-full h-full object-cover opacity-80"
         />
-<<<<<<< HEAD
         <div className="absolute inset-0 flex flex-col bg-black/30 gap-4 items-center justify-center  text-center text-black px-4">
-=======
-        <div className="absolute inset-0 flex flex-col gap-4 items-center justify-center mb-20 text-center text-black px-4">
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
           <h1 className="text-5xl font-bold mb-4">Turn your prep into power</h1>
           <p className="text-xl mb-4">with flashcards, mock tests, and study boosters</p>
           <p className="text-lg mb-6 italic">"Crack every exam with confidence"</p>
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as yup from "yup"
 import image from '../assets/crackit2.jpg'
-import { FaFacebook } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
+import { FaFacebook, FaApple } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from 'react-router-dom';
-
 
 function LoginPage() {
   const Forms = [
@@ -25,12 +23,9 @@ function LoginPage() {
 
   return (
     <div className='relative h-lvh w-full'>
+      <img src={image} alt="Background" className='absolute top-0 left-0 w-full h-full object-cover' />
 
-      <img src={image} alt="" className='absolute top-0 left-0 w-full h-full object-cover' />
-
-
-      <div className='absolute top-0 left-0 w-full h-full bg-opacity-60 flex items-center justify-center'>
-
+      <div className='absolute top-0 left-0 w-full h-full bg-opacity-60 bg-white flex items-center justify-center'>
         <Formik
           initialValues={{ UserOrEmail: "", Password: "" }}
           validationSchema={schemas}
@@ -38,23 +33,16 @@ function LoginPage() {
             console.log(values)
           }}
         >
-<<<<<<< HEAD
-          <Form className='h-fit bg-white  w-3/12 flex items-center justify-center flex-col gap-4 p-4 py-8 shadow-lg rounded-xl'>
-
-            <h1 className='text-purple-500 font-semibold text-3xl'>Welcome</h1>
-=======
-          <Form className='h-fit bg-white border-purple-600 border-2 w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 flex items-center justify-center flex-col gap-4 p-4 py-8 shadow-lg rounded-xl'>
-
+          <Form className='h-fit bg-white w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 flex items-center justify-center flex-col gap-4 p-4 py-8 shadow-lg rounded-xl'>
 
             <h1 className='text-black font-medium text-3xl sm:text-4xl md:text-5xl'>Welcome</h1>
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
 
             <div className='flex flex-col gap-8 w-10/12 mt-6'>
               {Forms.map((val, i) => (
                 <div key={i} className='flex flex-col gap-3'>
                   <label className='font-medium text-sm'>{val.placeholder}</label>
                   <Field
-                    className='bg-white w-full rounded-md p-2 py-3 outline-none border-2 border-gray-200  focus:border-0 focus:ring focus:ring-purple-900 placeholder:text-sm ' 
+                    className='bg-white w-full rounded-md p-2 py-3 outline-none border-2 border-gray-200 focus:ring focus:ring-purple-900 placeholder:text-sm'
                     name={val.name}
                     type={val.type}
                     placeholder={val.placeholder}
@@ -63,33 +51,15 @@ function LoginPage() {
                 </div>
               ))}
             </div>
-<<<<<<< HEAD
-            <div className='text-purple-500 cursor-pointer  text-sm  w-10/12 text-right'>Forget password?</div>
 
-            <Link to={'/app'} className=' w-full flex items-center justify-center'>
-              <button
-                className='  text-white border-2 bg-purple-500  rounded-lg p-3 w-10/12 shadow-lg hover:bg-white hover:text-purple-500 transition-all duration-300 cursor-pointer hover:font-bold delay-75 ease-in-out'
-                type="submit"
-              >
-                Log in
-              </button>
-            </Link>
-=======
-            <div className='text-red-700 cursor-pointer  w-10/12 text-right'>Forgot password?</div>
-          
-          
+            <div className='text-purple-500 cursor-pointer text-sm w-10/12 text-right'>Forgot password?</div>
+
             <button
-              className='bg-white  text-black border-2 border-purple-500  rounded-lg p-3 w-10/12 shadow-lg hover:bg-purple-400 hover:text-white transition'
+              className='text-white bg-purple-500 border-2 border-purple-500 rounded-lg p-3 w-10/12 shadow-lg hover:bg-white hover:text-purple-500 transition-all duration-300 cursor-pointer font-semibold'
               type="submit"
             >
-              <Link to={'/app'}>
               Log in
-              </Link>
             </button>
-          
-           
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
-
 
             <div className="w-10/12 h-[1px] bg-gray-600 mt-4 relative">
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 text-sm">
@@ -97,28 +67,18 @@ function LoginPage() {
               </div>
             </div>
 
-
-
             <div className='flex gap-4 text-2xl cursor-pointer'>
               <div className='text-blue-700'><FaFacebook /></div>
               <div><FaApple /></div>
               <div><FcGoogle /></div>
             </div>
 
-
-<<<<<<< HEAD
             <Link to={"/SignUp"}>
-=======
-              <Link to={"/SignUp"}>
->>>>>>> d489f9b7653f56ad4d8ed0a595da3765537316c9
-
-              <div className='mt-8 flex gap-3'>Don't have an account? <span className='underline text-purple-600 cursor-pointer'>Sign Up</span></div>
+              <div className='mt-8 flex gap-1 text-sm text-black'>
+                <span>Don't have an account?</span>
+                <span className='underline text-purple-600 cursor-pointer'>Sign Up</span>
+              </div>
             </Link>
-
-
-
-
-
           </Form>
         </Formik>
       </div>
